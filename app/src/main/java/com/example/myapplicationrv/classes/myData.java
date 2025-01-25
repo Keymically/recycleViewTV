7054dcb7c36479ec9aba48ce71b4a09ce1b6fb1d
ruthie <rottenkof@gmail.com>
2025-01-25 19:42:03 +0200

    proj backup

---
+++ app/src/main/java/com/example/myapplicationrv/classes/myData.java
@@ -0,0 +1,17 @@
+package com.example.myapplicationrv.classes;
+
+
+import com.example.myapplicationrv.R;
+
+public class myData {
+
+    public static String[] nameArray = {"Aang", "Katara", "Sokka", "Appa", "Zuko", "Toph", "Suki","Iroh", "Azula", "Ozai"};
+    public static String[] versionArray = {"Aang was an Air Nomad and the Avatar during the Hundred Year War", "Katara is a waterbending master, born in the Southern Water Tribe to Chief Hakoda and his wife Kya", "Water Tribe warrior of the Southern Water Tribe and the son of Chief Hakoda and Kya", "Appa was Avatar Aang's loyal sky bison, who shared a strong bond with him and Momo as they were all some of the few remaining traces of the Air Nomad civilization", "Zuko is a Fire Nation royal and firebending master who reigned as Fire Lord He is the eldest child and only son of Princess Ursa and Fire Lord Ozai", "Toph Beifong is an earthbending master, one of the most powerful of her time, and the discoverer of metalbending. Blind since birth, Toph was constantly treated condescendingly because of her visual impairment", "Suki was the leader of one of the Kyoshi Warrior bands of the Earth Kingdom's Kyoshi Island", "Iroh was a retired Fire Nation general, a former Crown Prince of the nation, a Grand Lotus of the Order of the White Lotus a firebending master, and a wise mentor to his nephew Zuko", "Azula was a princess of the Fire Nation, daughter of Fire Lord Ozai and Ursa, younger sister of Zuko A firebending prodigy, Azula was manipulative and obsessed with power. She was a skilled strategist who orchestrated the coup of the Earth Kingdom capital","Ozai was the ruler of the Fire Nation during the final years of the Hundred Year War, reigning as Fire Lord believed to be the most powerful firebender in the world"};
+
+    public static Integer[] drawableArray = {R.drawable.aang, R.drawable.katara, R.drawable.sokka,
+            R.drawable.appa, R.drawable.zuko, R.drawable.toph,
+            R.drawable.suki, R.drawable.iroh, R.drawable.azula,R.drawable.ozai};
+
+    public static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+}
